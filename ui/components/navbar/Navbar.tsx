5e864ca9bd8f955972e2b1351e1186650cbcb193
Dennis Höllmann <d.hoellmann@shopware.com>
2023-06-26 15:09:44 +0200

    CAR-1 - Improve navbar

--- ui/components/navbar/Navbar.tsx
+++ ui/components/navbar/Navbar.tsx
@@ -1,22 +1,34 @@
-import {View} from 'react-native';
-import {BLOCK_SPACING} from '../../../constants/SIZES';
+import {TouchableOpacity, View} from 'react-native';
+import {BLOCK_SPACING, FONT_SIZE} from '../../../constants/SIZES';
 import UI_Text from '../basic/Text';
+import {useRouter, useSegments} from 'expo-router';
+import {MaterialIcons} from '@expo/vector-icons';
+import {COLOR} from '../../../constants/COLORS';
 
 export default function UI_Navbar() {
+  const router = useRouter();
+  const segments = useSegments();
+
   return (
     <View style={{
       display: 'flex',
       flexDirection: 'row'
     }}>
-      <View style={{flex: 1, padding: BLOCK_SPACING.LG}}>
+      <TouchableOpacity style={{flex: 1, padding: BLOCK_SPACING.LG, justifyContent: 'center', alignItems: 'center'}}>
         <UI_Text center>Wartung</UI_Text>
-      </View>
-      <View style={{flex: 1, padding: BLOCK_SPACING.LG}}>
-        <UI_Text center href={'/home/'}>Übersicht</UI_Text>
-      </View>
-      <View style={{flex: 1, padding: BLOCK_SPACING.LG}}>
-        <UI_Text center href={'/fuel/'}>Spritt</UI_Text>
-      </View>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {router.push('/home/');}}style={{flex: 1, padding: BLOCK_SPACING.LG, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', opacity: segments.includes('home') ? 1 : 0}}>
+          <View style={{width: FONT_SIZE.XL + 10, height: FONT_SIZE.XL + 10, backgroundColor: COLOR.BTN_COLOR, borderRadius: FONT_SIZE.XL}}></View>
+        </View>
+        <MaterialIcons name="home" size={FONT_SIZE.XL} color={COLOR.FONT_PRIMARY} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {router.push('/fuel/');}}style={{flex: 1, padding: BLOCK_SPACING.LG, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', opacity: segments.includes('fuel') ? 1 : 0}}>
+          <View style={{width: FONT_SIZE.XL + 10, height: FONT_SIZE.XL + 10, backgroundColor: COLOR.BTN_COLOR, borderRadius: FONT_SIZE.XL}}></View>
+        </View>
+        <MaterialIcons name="local-gas-station" size={FONT_SIZE.XL} color={COLOR.FONT_PRIMARY} />
+      </TouchableOpacity>
     </View>
   );
 }
