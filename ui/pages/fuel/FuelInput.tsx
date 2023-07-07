c986a3000daf7e474c15363bdde7b11d35d5d1c9
Dennis Höllmann <d.hoellmann@shopware.com>
2023-07-07 15:15:41 +0200

    CAR-3 - add fuel station selector to new entry dialog

--- ui/pages/fuel/FuelInput.tsx
+++ ui/pages/fuel/FuelInput.tsx
@@ -1,18 +1,48 @@
 import {useEffect, useState} from 'react';
-import {type WebSQLDatabase} from 'expo-sqlite';
 import {View} from 'react-native';
 import UI_TextInput from '../../components/basic/TextInput';
-import {BLOCK_SPACING, FONT_SIZE, FONT_WEIGHT} from '../../../constants/SIZES';
+import {BLOCK_SPACING, FONT_SIZE} from '../../../constants/SIZES';
 import UI_Button from '../../components/basic/Button';
 import UI_Card from '../../components/container/Card';
 import UI_FuelArrayView from '../../components/fuel/FuelArrayView';
 import UI_Pager from '../../components/container/Pager';
 import UI_ModalPagerPage from '../../components/modal/ModalPagerPage';
 import {useDb} from '../../../app/_layout';
+import { Image } from 'expo-image';
 
 import * as Location from 'expo-location';
 import UI_Text from '../../components/basic/Text';
-import {type GasStation, getNearestGasStation} from '../../../utils/fuelApi';
+import {type GasStation, getImagePath, getNearestGasStations} from '../../../utils/fuelApi';
+import UI_Select, {UI_I_SelectItem} from '../../components/basic/Select/Select';
+
+function gasStationSelectList(stations: GasStation[]): UI_I_SelectItem[] {
+  return stations.map((e) => {
+    const path = getImagePath(e);
+    return {
+      value: e.id,
+      label: e.name,
+      element: <View style={{
+        flexDirection: 'row',
+        gap: BLOCK_SPACING.MD
+      }}>
+        <View style={{flex:1}}>
+          <Image
+            source={getImagePath(e)}
+            contentFit="contain"
+            transition={200}
+            style={{
+              flex: 1
+            }}
+          />
+        </View>
+        <View style={{flex:6}}>
+          <UI_Text>{e.name}</UI_Text>
+          <UI_Text size={FONT_SIZE.SM}>{`${e.street} ${e.houseNumber} - ${e.postCode} ${e.place}`}</UI_Text>
+        </View>
+      </View>
+    };
+  });
+}
 
 export default function PG_FuelInput({refreshItems}: {refreshItems: () => void}) {
   const [distance, setDistance] = useState<string>('');
@@ -22,8 +52,8 @@ export default function PG_FuelInput({refreshItems}: {refreshItems: () => void})
 
   const db = useDb();
 
-  const [location, setLocation] = useState<Location.LocationObject | null>(null);
-  const [nearestStation, setNearestStation] = useState<GasStation | null>(null);
+  const [nearestStations, setNearestStations] = useState<GasStation[]>([]);
+  const [selectedStation, setSelectedStation] = useState<number | string | null>(null);
 
   useEffect(() => {
     (async () => {
@@ -34,9 +64,11 @@ export default function PG_FuelInput({refreshItems}: {refreshItems: () => void})
       }
 
       const location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      const nStation = await getNearestGasStation(location.coords.latitude, location.coords.longitude);
-      setNearestStation(nStation);
+      const nStations = await getNearestGasStations(location.coords.latitude, location.coords.longitude);
+      setNearestStations(nStations);
+      if (nStations.length > 0) {
+        setSelectedStation(nStations[0].id);
+      }
     })();
   }, []);
 
@@ -68,6 +100,8 @@ export default function PG_FuelInput({refreshItems}: {refreshItems: () => void})
         <UI_TextInput title={'Distanz'} unit={'km'} setValue={setDistance} value={distance} placeholder={'Distanz'}/>
         <UI_TextInput title={'Verbrauch'} unit={'l'} setValue={setConsumption} value={consumption} placeholder={'Verbrauch'}/>
         <UI_TextInput title={'Kosten'} unit={'€'} setValue={setCost} value={cost} placeholder={'Kosten'}/>
+
+        <UI_Select selected={selectedStation} setSelected={setSelectedStation} data={gasStationSelectList(nearestStations)} title={'Tankstelle'}/>
       </View>
     </UI_ModalPagerPage>
 
@@ -82,16 +116,6 @@ export default function PG_FuelInput({refreshItems}: {refreshItems: () => void})
         <UI_Card>
           <UI_FuelArrayView distance={parseFloat(distance)} consumption={parseFloat(consumption)} cost={parseFloat(cost)} open={true} />
         </UI_Card>
-        {
-          nearestStation != null ?
-            <UI_Card>
-              <UI_Text size={FONT_SIZE.LG} weight={FONT_WEIGHT.LG}>Tankstelle</UI_Text>
-              <UI_Text>{nearestStation.name}</UI_Text>
-              <UI_Text weight={FONT_WEIGHT.SM} size={FONT_SIZE.SM}>{nearestStation.brand}</UI_Text>
-            </UI_Card>
-            :
-            <></>
-        }
       </View>
     </UI_ModalPagerPage>
   </UI_Pager>;
--- ui/pages/fuel/FuelList.tsx
+++ ui/pages/fuel/FuelList.tsx
@@ -2,7 +2,7 @@ import UI_Text from '../../components/basic/Text';
 import {COLOR} from '../../../constants/COLORS';
 import UI_FuelArrayView from '../../components/fuel/FuelArrayView';
 import UI_Card from '../../components/container/Card';
-import {FONT_SIZE} from '../../../constants/SIZES';
+import {BLOCK_SPACING, FONT_SIZE} from '../../../constants/SIZES';
 import {View} from 'react-native';
 
 export interface fuelListEntry {
@@ -16,20 +16,20 @@ export default function PG_FuelList({list}: {list: fuelListEntry[]}): JSX.Elemen
   let lastDate:Date | null = null;
 
   return <>{list.map((element, index) => {
-    let d = <View key={`date_${index}`}></View>;
+    let d = <></>;
     const newDate = new Date(element.date);
     if (lastDate === null || lastDate.getMonth() !== newDate.getMonth()) {
       d = <UI_Text key={`date_${index}`}>{newDate.toLocaleString('default', { month: 'long', year: 'numeric' })}</UI_Text>;
     }
     lastDate = newDate;
     return (
-      <>
+      <View key={index} style={{gap: BLOCK_SPACING.SM}}>
         {d}
-        <UI_Card key={index}>
+        <UI_Card>
           <UI_Text size={FONT_SIZE.XS} color={COLOR.FONT_SECONDARY}>{new Date(element.date).toLocaleString()}</UI_Text>
           <UI_FuelArrayView distance={element.distance} consumption={element.consumption} cost={element.cost} />
         </UI_Card>
-      </>
+      </View>
     );
   })}</>;
 }
